625ab83310fa3a2a341414c56e62c9aa6c3b6319
cm-kato-jun <kato.jun@classmethod.jp>
2017-02-21 08:55:05 +0900

    failure test case

--- VSMCDemo/VSMCDemoTests/VSMCDemoTests.swift
+++ VSMCDemo/VSMCDemoTests/VSMCDemoTests.swift
@@ -24,6 +24,8 @@ class VSMCDemoTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+        XCTAssertEqual(1 + 1, 3)
     }
     
     func testPerformanceExample() {
